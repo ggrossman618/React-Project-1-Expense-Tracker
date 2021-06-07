2d51eaec7b6f856eca82bd627c158c8499af3b0b
ggrossman618 <ggrossman@colgate.edu>
2021-06-07 17:09:34 -0400

    React States Part 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ ex. ExpenseItem
 */
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -29,7 +30,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,19 @@
 function ***
 */
 
-import ExpenseDate from './ExpenseDate.js';
-import Card from '../UI/Card.js'
+import React, { useState } from 'react';
+import ExpenseDate from "./ExpenseDate.js";
+import Card from "../UI/Card.js";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   /* props can be named anything, holds input parameters*/
+  const [title, setTitle] = useState(props.title);
+
+  function clickHandler() {
+    setTitle('Updated!');
+    console.log('Updated!')
+  }
 
   return (
     //you cannot return multiple things, so wrap everything in <div>
@@ -17,11 +24,13 @@ function ExpenseItem(props) {
     // <ExpenseDate></ExpenseDate> = <ExpenseDate>, no paraemeter in the middle
 
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/> {/*passing props.date to expenseDate*/} 
+      <ExpenseDate date={props.date} /> {/*passing props.date to expenseDate*/}
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>{" "}
+      {/*you don't execute function using (), you just point to it */}
     </Card>
   );
 }
